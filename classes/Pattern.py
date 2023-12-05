83e42ae9fdca8257fdb3416059e878132dd9bc60
driesvanspauwen <dries.vanspauwen@gmail.com>
2023-12-05 20:40:16 +0000

    fix naming in pattern

--- classes/Pattern.py
+++ classes/Pattern.py
@@ -43,10 +43,10 @@ if __name__ == "__main__":
         sanitizers=["sanitize_something"],
         sinks=["some_sink"]
     )
-    # Testing is_* functions
-    print("Is 'user_input' a source?", vulnerability_pattern.is_source("user_input"))
-    print("Is 'sanitize_sql_query' a sanitizer?", vulnerability_pattern.is_sanitizer("sanitize_sql_query"))
-    print("Is 'my_sink' a sink?", vulnerability_pattern.is_sink("my_sink"))
+    # Testing contains_* functions
+    print("Is 'user_input' a source?", vulnerability_pattern.contains_source("user_input"))
+    print("Is 'sanitize_sql_query' a sanitizer?", vulnerability_pattern.contains_sanitizer("sanitize_sql_query"))
+    print("Is 'my_sink' a sink?", vulnerability_pattern.contains_sink("my_sink"))
     # Testing getter
     print(f"Get the sanitizers: {vulnerability_pattern.get_sanitizers()}")
     print(f"Get the sinks: {vulnerability_pattern.get_sinks()}")
