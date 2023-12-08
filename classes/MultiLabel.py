9a75a5081778c8f1752c39ed6f55aca18bbb462d
driesvanspauwen <dries.vanspauwen@gmail.com>
2023-12-08 17:42:30 +0000

    lab 2 fix policy mapping

--- classes/MultiLabel.py
+++ classes/MultiLabel.py
@@ -3,29 +3,29 @@ from Label import Label
 
 class MultiLabel:
     def __init__(self, labels_dict = {}):
-        self.pattern_to_labels_mapping = labels_dict # {pattern_name: label1}
+        self.pattern_to_label_mapping = labels_dict # {pattern_name: (pattern, label)}
     
-    def get_pattern_to_labels_mapping(self):
-        return self.pattern_to_labels_mapping
+    def get_pattern_to_label_mapping(self):
+        return self.pattern_to_label_mapping
 
-    def get_labels(self, pattern_name):
-        return self.pattern_to_labels_mapping[pattern_name]
+    def get_label(self, pattern):
+        return self.pattern_to_label_mapping[pattern.get_name()][1]
 
-    def get_patterns(self):
-        return self.pattern_to_labels_mapping.keys()   
+    def get_pattern_names(self):
+        return list(self.pattern_to_label_mapping.keys())
 
-    def add_label(self, pattern_name, label):
-        if pattern_name in self.pattern_to_labels_mapping:
+    def add_label(self, pattern, label):
+        if pattern.get_name() in self.pattern_to_label_mapping:
             print("Pattern already excist.")
         else:
-            self.pattern_to_labels_mapping[pattern_name] = {label}
+            self.pattern_to_label_mapping[pattern.get_name()] = (pattern, label)
 
     def combine(self, other_multilabel):
-        result = MultiLabel(self.get_pattern_to_labels_mapping())
-        for pattern, label in other_multilabel.get_pattern_to_labels_mapping().items():
+        result = MultiLabel(self.get_pattern_to_label_mapping())
+        for pattern_name, (pattern, label) in other_multilabel.get_pattern_to_label_mapping().items():
             result.add_label(pattern, label)
         
-        # for pattern, labels in other_multilabel.pattern_to_labels_mapping.items():
+        # for pattern, labels in other_multilabel.pattern_to_label_mapping.items():
         #     for label in labels:
         #         result.add_label(pattern, label)
         return result
@@ -35,24 +35,24 @@ if __name__ == "__main__":
     label1 = Label([("sourceX", {"SanA", "SanB"}), ("SourceY", {"SanH", "SanM"})])
     label2 = Label([("SourceZ", {"SanC", "SanD"}), ("SourceE", {"SanW", "SanQ"})])
 
+    pattern1 = Pattern("pattern1", ["sourceX", "SourceY"], [], [])
+    pattern2 = Pattern("pattern2", ["SourceZ", "SourceE"], [], [])
 
-    multi_label = MultiLabel({"pattern1":label1, "pattern2":label2})
+    multi_label = MultiLabel({pattern1.get_name():(pattern1, label1), pattern2.get_name():(pattern2, label2)})
     
-    label  = multi_label.get_labels("pattern1")
+    label  = multi_label.get_label(pattern1)
     assert(label.get_source_names() == ['sourceX', 'SourceY'])
     assert(label.get_sanitizers("sourceX") == {"SanA", "SanB"})
     
-    label = multi_label.get_labels("pattern2")
+    label = multi_label.get_label(pattern2)
     assert(label.get_source_names() == ['SourceZ', 'SourceE'])
     assert(label.get_sanitizers("SourceZ") == {"SanC", "SanD"})
 
-    
     label3 = Label([("source5", {"sanG"})])
 
-    multi_label2 = MultiLabel({"pattern7":label3})
-    multi_label_combi = multi_label.combine(multi_label2)
+    pattern3 = Pattern("pattern3", ["sanG"], [], [])
 
-    assert(list(multi_label_combi.get_patterns()) == ['pattern1', 'pattern2', 'pattern7'])
-    # assert that both labels are in combination
-    # assert any(label.get_source_names() == ["sourceX", "SourceY"] in multi_label_combi.get_labels("pattern1").get_source_names())
-    # assert any(label.get_source_names() == ["SourceZ", "SourceE"] for label in multi_label_combi.get_labels("pattern2"))
+    multi_label3 = MultiLabel({pattern3.get_name():(pattern3, label3)})
+    multi_label_combi = multi_label.combine(multi_label3)
+
+    assert(multi_label_combi.get_pattern_names()) == ['pattern1', 'pattern2', 'pattern3']
